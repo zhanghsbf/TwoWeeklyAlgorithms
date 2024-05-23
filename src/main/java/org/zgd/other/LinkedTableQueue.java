967bd5d71e5381cbd16cdbfa6e8fda1a04867d61
zyk <1019437875@qq.com>
2024-05-23 19:48:42 +0800

    123

---
+++ src/main/java/org/zgd/other/LinkedTableQueue.java
@@ -0,0 +1,37 @@
+package org.zgd.other;
+
+import sun.plugin.javascript.navig.AnchorArray;
+
+/**
+ * @description: TODO
+ * @date: 2024/5/23 17:40
+ * @author: zyk
+ */
+public class LinkedTableQueue {
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class ArrayQueue{
+
+    // 基础数组队列功能, 入队, 出队
+    private String[] items;
+    private int n = 0;
+    private int head = 0;
+    private int tail = 0;
+
+    public ArrayQueue(int capacity){
+        items = new String[capacity];
+    }
+    public void enqueue(String item){
+        
+    }
+
+    public String dequeue(){
+        return null;
+    }
+
+}
